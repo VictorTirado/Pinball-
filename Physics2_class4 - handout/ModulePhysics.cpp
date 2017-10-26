55ac3bc8ba1e41a3d3f42efad48717398e0bee44
manelmourelo <manelmourelo.1@gmail.com>
2017-10-26 10:00:38 +0200

    Ball

--- Physics2_class4 - handout/ModulePhysics.cpp
+++ Physics2_class4 - handout/ModulePhysics.cpp
@@ -71,6 +71,7 @@ PhysBody* ModulePhysics::CreateCircle(int x, int y, int radius)
 	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
 
 	b2Body* b = world->CreateBody(&body);
+	b->SetBullet(true);
 
 	b2CircleShape shape;
 	shape.m_radius = PIXEL_TO_METERS(radius);
