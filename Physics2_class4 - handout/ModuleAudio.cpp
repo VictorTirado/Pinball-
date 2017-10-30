c0bb876b2bda907bcc487f2c759ec3251fd0c18f
manelmourelo <manelmourelo.1@gmail.com>
2017-10-30 19:54:31 +0100

    MusicFix

--- Physics2_class4 - handout/ModuleAudio.cpp
+++ Physics2_class4 - handout/ModuleAudio.cpp
@@ -7,7 +7,8 @@
 #pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
 
 ModuleAudio::ModuleAudio(Application* app, bool start_enabled) : Module(app, start_enabled), music(NULL)
-{}
+{
+}
 
 // Destructor
 ModuleAudio::~ModuleAudio()
